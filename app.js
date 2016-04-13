53181f0e017c26c5e850113ce79ab38a6e8bd6f1
Patrik Johnson <patrik@zebros.fi>
2016-04-13 19:56:55 +0300

    Add thingamabob and maquette libraries, implement initial skeleton components

--- app.js
+++ app.js
@@ -1,23 +1,69 @@
-var currNotification;
-
-function showNotification() {
-  if (Notification.permission !== 'denied') {
-    if (Notification.permission === 'granted') {
-      currNotification = new Notification('Hello thingie');
-    } else {    
-      Notification.requestPermission(function(permission) { // would return promise w/o callback
-        console.log('requestPermission result: ' + permission);
-        showNotification();
-      });
-    } 
-  } else {
-    alert('Notifications blocked');
+var Demo = {};
+Demo.projector = maquette.createProjector();
+Demo.state = new StateMachine({
+  initial: window.Notification ? window.Notification.permission : 'unsupported',
+  states: {
+    'default'     : ['denied', 'granted'],
+    'denied'      : ['default', 'granted'],
+    'granted'     : ['default', 'denied'],
+    'unsupported' : []
   }
+});
+
+if (Demo.state.current !== 'unsupported') {
+  setInterval(function() {
+    if (Notification.permission !== Demo.state.current)
+      Demo.state.go(Notification.permission);
+  }, 1000);
 }
 
-if (!window.Notification) {
-  showNotification = function() {
-    alert('Notifications not available in this browser');
-  };
+Demo.state.on('all', function() {
+  Demo.projector.scheduleRender();
+})
+
+Demo.requestPermission = Notification.requestPermission.bind({}, Demo.state.go);
+Demo.showNotification = function() {
+  Demo.notification = new Notification('Hello notification!');
 }
 
+var defaultView = function() {
+  var h = maquette.h;
+  return h('button', { onclick : Demo.requestPermission }, [
+     'Enable Notifications'
+   ]);
+};
+
+var deniedView = function() {
+  var h = maquette.h;
+  return h('div', [
+     'denied'
+   ]);
+};
+
+var grantedView = function() {
+  var h = maquette.h;
+  return h('button', { onclick : Demo.showNotification }, [
+     'Show Notifications'
+   ]);
+};
+
+var unsupportedView = function() {
+  var h = maquette.h;
+  return h('div', [
+     'unsupported'
+   ]);
+};
+
+
+window.addEventListener('load', function() {
+  var components = {
+    default     : defaultView,
+    denied      : deniedView,
+    granted     : grantedView,
+    unsupported : unsupportedView,
+  }
+
+  Demo.projector.replace(document.getElementById('app'), function() {
+    return maquette.h('div', [ components[Demo.state.current]() ]);
+  });
+});
