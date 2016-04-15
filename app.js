76af6b564f2f75475d17a424afc39b81ecf98d1e
Patrik Johnson <patrik@zebros.fi>
2016-04-15 23:44:53 +0300

    Update granted view to support reading notification content from form instead of hardcoding

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ Demo.state = new StateMachine({
     'unsupported' : []
   }
 });
+Demo.notifications = [];
+Demo.form = {
+  title : '',
+  dir   : 'auto',
+  body  : '',
+  tag   : '',
+  icon  : '',
+  data  : {}
+};
+Demo.helpers = {};
+Demo.helpers.setValue = function(obj, field) {
+  return function(evt) { obj[field] = evt.target.value; };
+};
+
+Demo.helpers.titleHandler = Demo.helpers.setValue(Demo.form, 'title');
+Demo.helpers.bodyHandler  = Demo.helpers.setValue(Demo.form, 'body');
 
 if (Demo.state.current !== 'unsupported') {
   setInterval(function() {
@@ -23,12 +39,27 @@ Demo.state.on('all', function() {
 
 Demo.requestPermission = Notification.requestPermission.bind({}, Demo.state.go);
 Demo.showNotification = function() {
-  Demo.notification = new Notification('Hello notification!');
+  var notif = new Notification(Demo.form.title, {
+    body : Demo.form.body,
+    // dir
+    // lang
+    // tag
+    // icon
+    // data
+  });
+  notif.addEventListener('close', function() {
+    var index = Demo.notifications.indexOf(notif);
+    if (index > -1)
+      Demo.notifications.splice(index, 1);
+    notif.removeEventListener('close', this);
+  });
+
+  Demo.notifications.push(notif);
 }
 
 var defaultView = function() {
   var h = maquette.h;
-  return h('button', { onclick : Demo.requestPermission }, [
+  return h('button', { onclick : Demo.requestPermission, key: 'default' }, [
      'Enable Notifications'
    ]);
 };
@@ -42,8 +73,28 @@ var deniedView = function() {
 
 var grantedView = function() {
   var h = maquette.h;
-  return h('button', { onclick : Demo.showNotification }, [
-     'Show Notifications'
+  return  h('form', { onsubmit: 'return false' }, [
+            h('div', [
+              'title:',
+              h('input', {
+                type: 'text', value: Demo.form.title, oninput: Demo.helpers.titleHandler
+              }),
+            ]),
+            h('div', [
+              'body:',
+              h('input', {
+                type: 'text', value: Demo.form.body, oninput: Demo.helpers.bodyHandler
+              })
+            ]),
+            h('pre', [
+              'new Notification(\'' + Demo.form.title + '\', {\n' +
+                '    body: \'' + Demo.form.body + '\',\n' +
+                '    dir: \'auto,\n' +
+              '})' + JSON.stringify(Demo.form, null, 4)
+            ]),
+            h('button', { onclick : Demo.showNotification, key: 'granted' }, [
+              'Show Notification'
+            ]),
    ]);
 };
 
